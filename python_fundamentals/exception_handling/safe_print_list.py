a962eb0f6afbae08996fe5091d282b15d5b901c3
layan <layanalghymah@outlook.sa>
2026-09-20 18:46:35 +0200

    Fixed pycodestyle

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -2,14 +2,14 @@
 
 def safe_print_list(my_list=[], x=0):
 
-	count = 0
+    count = 0
 
-	for (i) in range(x):
-		try:
-			print(my_list[i], end="")
-			count += 1
-		except IndexError:
-			break
+    for (i) in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+                except IndexError:
+                    break
 
-	print()
-	return count
+        print()
+        return count
