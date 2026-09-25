dc84615503a8ecf238da506662779ebcea5ef636
layan alghymah <layanalghymah@outlook.sa>
2026-09-25 14:02:28 +0200

    Add Square size attribute

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square with a private size."""
+        self.__size = size
