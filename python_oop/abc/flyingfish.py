65b4e753fdecc824048cb2949afe8ed31264df56
layan alghymah <layanalghymah@outlook.sa>
2026-09-26 21:47:17 +0200

    Add task 2

---
+++ python_oop/abc/flyingfish.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Demonstrate multiple inheritance using fish and bird classes."""
+
+
+class Fish:
+    """Represent a fish."""
+
+    def swim(self):
+        """Print the swimming behavior of a fish."""
+        print("The fish is swimming")
+
+    def habitat(self):
+        """Print the habitat of a fish."""
+        print("The fish lives in water")
+
+
+class Bird:
+    """Represent a bird."""
+
+    def fly(self):
+        """Print the flying behavior of a bird."""
+        print("The bird is flying")
+
+    def habitat(self):
+        """Print the habitat of a bird."""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """Represent a flying fish."""
+
+    def fly(self):
+        """Print the flying behavior of a flying fish."""
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """Print the swimming behavior of a flying fish."""
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """Print the habitat of a flying fish."""
+        print("The flying fish lives both in water and the sky!")
